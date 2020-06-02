e08c3afb698f716576621dc5a80dce7b0b04e953
umbreon22 <msw469@gmail.com>
2020-06-02 03:12:23 -0700

    Opened module and exported additional packages in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module novel {
+open module novel {
 
     requires jdk.unsupported;
     requires org.slf4j;
@@ -6,8 +6,11 @@ module novel {
     exports novel.api;
     exports novel.api.types.read;
     exports novel.api.types.write;
+    exports novel.api.types.write.pens;
     exports novel.api.types.factory;
     exports novel.api.types.adapt;
     exports novel.api.types.token;
+    exports novel.api.types.annotations;
+    exports novel.api.streams;
 
 }
