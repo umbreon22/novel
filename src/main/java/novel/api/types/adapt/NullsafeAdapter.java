23ccefcf73bd707719c85ed68a565c4b56180c5e
umbreon22 <msw469@gmail.com>
2020-06-02 03:10:53 -0700

    Improved NullsafeAdapter documentation

--- src/main/java/novel/api/types/adapt/NullsafeAdapter.java
+++ src/main/java/novel/api/types/adapt/NullsafeAdapter.java
@@ -1,11 +1,13 @@
 package novel.api.types.adapt;
 
+import novel.api.types.annotations.Inked;
 import novel.api.types.read.DataPaper;
 import novel.api.types.read.DataPaperReadException;
 import novel.api.types.write.pens.DataPen;
 
 /**
  * A wrapped class for {@link ObjectDataAdapter} that performs a null safe read/write.
+ * This ignores {@link Inked} annotations for the adapted class only. todo: should this flow downstream?
  */
 final class NullsafeAdapter<T> implements ObjectDataAdapter<T> {
 
